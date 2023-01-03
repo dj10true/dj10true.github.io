c262ba4ff2da5b175dd724919912e2324063c4a3
dj10true <106715663+dj10true@users.noreply.github.com>
2023-01-03 16:21:36 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,25 @@
+icon.onclick = function(){
+   icon.style.display = "hidden";  
+    Doll_body.style.display = "hidden";
+      Doll_body.className = "modal_body";
+      Maus.src = src="./img/maus full.png" 
+}
+ 
+
+
+
+
+
+
+
+// var modal = document.getElementById('icon');
+// var modalImg = document.getElementById("Maus");
+// icon.onclick = function(){
+//    Maus.style.display = "";
+//     Maus.src =  img="img/maus full.png";
+// }
+ 
+
+// Maus.onclick = function() {
+//        Maus.style.display = "none";
+//       Maus.className = "Doll";
